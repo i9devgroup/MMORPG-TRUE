04f39a04387fdec3332e31f6255a7cd610866548
Petryck WIlliam <dev@i9devgroup.com>
2022-05-03 13:43:28 -0300

    configurado live-server

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const path = require("path");
 const port = 3002;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/home.html'))
-  })
+    res.sendFile(path.join(__dirname, './public/website/home.html'))
+})
+
+app.get('/game', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/game/index.html'))
+})
 
 http.createServer(app).listen(port, () =>
-    console.log(`Servidor Carregado http://localhost:${port}`));
+    console.log(`Servidor Carregado http://localhost:${port}`)
+  );
