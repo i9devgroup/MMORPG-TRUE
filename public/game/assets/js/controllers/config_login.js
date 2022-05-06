00ee8ebbfa7f2fa0e43136d3bc05e41a253cf907
Petryck WIlliam <104837591+i9devgroup@users.noreply.github.com>
2022-05-06 13:56:11 -0300

    .
    .

---
+++ public/game/assets/js/controllers/config_login.js
@@ -0,0 +1,47 @@
+var channel = geckos({ port: 6363 })
+
+channel.onConnect(function (error) {
+    if (error) {
+      console.error(error.message)
+    } else {
+      console.log("You're connected on MMORPG")
+    }
+  })
+
+$(document).on('click', '#login-account', function(e){
+    e.preventDefault()
+    var userName = $('#UserLogin').val()
+    var password = $('#PassLogin').val()
+
+    if(userName != '' && password != ''){
+        var login = {
+            username:userName,
+            password:password
+        }
+    
+        $(this).attr('disabled', true)
+    
+        window.channel.emit('QueryLogin', login)
+    }
+    
+
+
+
+})
+
+
+
+channel.on('StatusLogin', (data) => {
+        console.log(data)
+    if(data.status == true){
+        localStorage.setItem('Account', JSON.stringify(data));
+        window.location.href = "/game";
+    }else{
+
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+
+        $('#login-account').attr('disabled', false)
+    }
+    
+})
